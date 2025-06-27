352a49bb9e771a74708d2b4764c997755e452c46
vs213 <victoramarals25y2@gmail.com>
2025-06-27 20:18:08 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MapaFocosIncendio from "./MapaFocosIncendio";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <MapaFocosIncendio />
+  </React.StrictMode>
+);
